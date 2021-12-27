420e9a75912e72032e7690709a9139a240d133a1
Maciej Aleksandrowicz <macvek@gmail.com>
2021-12-27 20:18:44 +0100

    initial

---
+++ DevScripts/runInDocker.ps1
@@ -0,0 +1,1 @@
+docker run -d -p 80:80 --rm --name blocks -v ${pwd}/../www:/var/www php:8.1-rc-apache
---
+++ www/html/index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        Welcome to <?= "Blocks" ?>
+
+    </body>
+</html>
